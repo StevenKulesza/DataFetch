a2463756e53de0bb3ab4dbd0a920bbac12c1930b
Steven Kulesza <stevenn.kulesza@gmail.com>
2017-12-12 16:08:45 -0500

    map values over array with fetch api

--- App/src/js/App.js
+++ App/src/js/App.js
@@ -3,27 +3,63 @@ import Collapsible from './collapsible'
 import image from '../images/expand-vertical-4.svg';
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      isLoading: true,
+      contacts: []
+    }
+  }
+
+  componentDidMount(){
+    this.fetchData();
+  }
+
+  fetchData(){
+    fetch('https://randomuser.me/api/?results=50&nat=us,dk,fr,gb')
+    .then(response => response.json())
+    .then(parsedJSON => parsedJSON.results.map(user =>  (
+      {
+        name: `${user.name.first} ${user.name.last}`,
+        username: `${user.login.username}`,
+        email: `${user.email}`,
+        location: `${user.location.street}, ${user.location.city}`
+      }
+    )))
+    .then(contacts => this.setState({
+      contacts,
+      isLoading: false
+    }))
+    .catch(error => console.log('parsing failed', error))
+  }
+
     render() {
-        return (
-            <div>
-                <header>
-                    <h1>The header</h1>
-                </header>
-                <div className="content">
-                    <div className="panel-group">
-                      <Collapsible title='Overview'>
-                        <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.</p>
-                      </Collapsible>
-                      <Collapsible title='Features'>
-                        <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.</p>
-                      </Collapsible>
-                      <Collapsible title='Software'>
-                        <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.</p>
-                      </Collapsible>
-                    </div>
-                </div>
-            </div>
-        );
+      const {isLoading, contacts} = this.state;
+
+      return (
+          <div>
+              <header>
+                  <h1>The header</h1>
+              </header>
+              <div className={`content ${isLoading ? 'is-loading' : ''}`}>
+                  <div className="panel-group">
+                    {
+                      !isLoading && contacts.length > 0 ? contacts.map(contact => {
+                        const {username, name, email, location} = contact;
+                        return <Collapsible key={username} title={name}>
+                          <p>{email}<br/> {location}</p>
+                        </Collapsible>
+                      }) : null
+                    }
+                  </div>
+                  <div className='loader'>
+                    <div className='icon'></div>
+                  </div>
+              </div>
+          </div>
+      );
     }
 }
 
